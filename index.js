74612996b6e76df68202ea5cde66e0b41068be6d
Simran <mdrsimran@gmail.com>
2021-06-08 13:56:15 +0545

    insert last

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ class LinkedList {
     //Insert first node
     insertFirst(data) {
         this.head = new Node(data, this.head);
+        this.size++;
+    }
+
+    //Insert last node
+    inserLast(data) {
+        let node = new Node(data);
+        let current;
+
+        //If empty, make head
+        if (!this.head) {
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+
+        this.size++;
     }
 
     //Print list data
@@ -32,5 +53,6 @@ const ll = new LinkedList();
 ll.insertFirst(100);
 ll.insertFirst(200);
 ll.insertFirst(300);
+ll.inserLast(400);
 
 ll.printListData();
