29ec3d521033ad9512b44246b7bcc664794a492e
Simran <mdrsimran@gmail.com>
2021-06-08 13:43:17 +0545

    change files

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-export default class Node {
+class Node {
     constructor(data, next = null) {
         this.data = data;
         this.next = next;
     }
 }
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    //Insert first node
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+}
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+console.log(ll);
